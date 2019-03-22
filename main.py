e2ac71c31df995c608c621177b0a0fe172eb0fb5
Stadzior <Stadzior@gmail.com>
2019-03-22 20:36:59 +0100

    Added .gitignore and moved data files.

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 from __future__ import print_function
-import keras
-from keras.datasets import mnist
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
@@ -12,19 +10,13 @@ num_classes = 10
 epochs = 2
 
 # input image dimensions
-img_rows, img_cols = 28, 28
+img_rows, img_cols = 512, 512
 
 # the data, split between train and test sets
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-
-if K.image_data_format() == 'channels_first':
-    x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
-    x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
-    input_shape = (1, img_rows, img_cols)
-else:
-    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
-    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
-    input_shape = (img_rows, img_cols, 1)
+(x_train, y_train), (x_test, y_test) = 
+x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+input_shape = (img_rows, img_cols, 1)
 
 x_train = x_train.astype('float32')
 x_test = x_test.astype('float32')
