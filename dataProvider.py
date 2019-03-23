e2c1ba7ea0d6eb20555478b4ef84aa3f1cb8c694
Stadzior <Stadzior@gmail.com>
2019-03-23 22:25:40 +0100

    Working with data generators.

--- dataProvider.py
+++ dataProvider.py
@@ -4,32 +4,23 @@ import numpy as np
 layer_dimensions = (512, 512)
 layer_size = layer_dimensions[0]*layer_dimensions[1]
 mask_suffix = "_Delmon_CompleteMM"
-img_mask_isLung = []
 
-def load_data(images_path, images_datatype, masks_path, masks_datatype):
-    for filename in filter(lambda x: x.endswith(".raw"), os.listdir(images_path)):        
-        img_layered = load_image('{}/{}'.format(images_path, filename), images_datatype)
-        mask_layered = load_image('{}/{}')
-
-
+def load_data(image_path, image_datatype, mask_path, mask_datatype, file_count):
+        data = []
+        for filename in list(map(lambda x: x.replace(".raw", ""), filter(lambda x: x.endswith(".raw"), os.listdir(image_path))))[:file_count]:  
+                print(filename)
+                data += load_file(filename, image_path, image_datatype, mask_path, mask_datatype)
+        return data
+        
 def load_image(path, datatype):   
-    img_file = open(path)
-    img_str = img_file.read()
-    img_vector = np.fromstring(img_str, dtype=datatype) 
-    layers_count = int(len(img_vector) / layer_size)
-    return np.reshape(img_vector, (layers_count, layer_size))
+        img_file = open(path, "rb")
+        img_str = img_file.read()
+        img_vector = np.fromstring(img_str, dtype=datatype) 
+        layers_count = int(len(img_vector) / layer_size)
+        return np.reshape(img_vector, (layers_count, layer_size))
 
-def load_images(path, datatype):
-    for file in filter(lambda x: x.endswith(".raw"), os.listdir()):
-        f = open(file, 'rb')
-        img_str = f.read()
-            
-        # converting to a int16 numpy array
-        ct_image_as_vector = np.fromstring(img_str, dtype=np.int8) if "MM" in file else np.fromstring(img_str, np.int16)
-
-        #adding to 
-
-
-
-def load_masks():
-    for file in filter(lambda x: x.endswith(".raw"), os.listdir("./masks")):
+def load_file(filename, image_path, image_datatype, mask_path, mask_datatype):
+        images = load_image("{}/{}.raw".format(image_path, filename), image_datatype)
+        masks = load_image("{}/{}{}.raw".format(mask_path, filename, mask_suffix), mask_datatype)
+        img_mask_tuples = list(zip(images, masks))
+        return img_mask_tuples
--- main.py
+++ main.py
@@ -5,34 +5,20 @@ from keras.layers import Conv2D, MaxPooling2D
 from keras import backend as K
 import model
 import dataProvider
+import numpy as np
 
-batch_size = 128
-num_classes = 10
-epochs = 2
+# batch_size = 128
+# num_classes = 10
+# epochs = 2
 
-# input image dimensions
-img_rows, img_cols = 512, 512
+# # input image dimensions
+# img_rows, img_cols = 512, 512
 
-# the data, split between train and test sets
-(x_train, y_train), (x_test, y_test) = dataProvider.load_data("./images", "./masks")
-x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
-x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
-input_shape = (img_rows, img_cols, 1)
+data = dataProvider.load_data("./images", np.int16, "./masks", np.int8, 5)
+data2 = "hue"
 
-x_train = x_train.astype('float32')
-x_test = x_test.astype('float32')
-x_train /= 255
-x_test /= 255
-print('x_train shape:', x_train.shape)
-print(x_train.shape[0], 'train samples')
-print(x_test.shape[0], 'test samples')
+# model = model.unet()
 
-# convert class vectors to binary class matrices
-y_train = keras.utils.to_categorical(y_train, num_classes)
-y_test = keras.utils.to_categorical(y_test, num_classes)
-
-model = model.unet()
-
-score = model.evaluate(x_test, y_test, verbose=0)
-print('Test loss:', score[0])
-print('Test accuracy:', score[1])
+# score = model.evaluate(x_test, y_test, verbose=0)
+# print('Test loss:', score[0])
+# print('Test accuracy:', score[1])
